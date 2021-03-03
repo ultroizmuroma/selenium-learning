63b83b5cbb2fced6644672cf76b9dee70bb75de2
Алексей Евгеньевич Баринов <barinovale@mosreg.ru>
2021-03-03 23:12:55 +0300

    Фикстуры по закрытию браузера после выполнения тестов

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,34 +1,40 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    yield browser
+    browser.close()
 
 
-def test_something():
-    rosbank = go_to_page("https://www.rosbank-auto.ru/")
+def test_something(browser):
+    rosbank = go_to_page(browser, "https://www.rosbank-auto.ru/")
     assert rosbank[1] < 100
 
-    go_to_page("https://www.gosuslugi.ru/")
-    search_in_yandex("Поиск раз")
-    search_in_yandex("Поиск два")
+    go_to_page(browser, "https://www.gosuslugi.ru/")
+    search_in_yandex(browser, "Поиск раз")
+    search_in_yandex(browser, "Поиск два")
 
 
-def go_to_page(url) -> list:
+def go_to_page(browser, url) -> list:
     browser.get(url)
-    return calculate_for_page(url)
+    return calculate_for_page(browser, url)
 
 
-def search_in_yandex(search_string):
-    go_to_page("https://yandex.ru")
+def search_in_yandex(browser, search_string):
+    go_to_page(browser, "https://yandex.ru")
     search_field = browser.find_element(By.ID, "text")
     search_field.send_keys(search_string)
     search_field.send_keys(Keys.RETURN)
-    calculate_for_page(f"https://yandex.ru/ and {search_string}")
+    calculate_for_page(browser, f"https://yandex.ru/ and {search_string}")
 
 
-def calculate_for_page(url) -> list:
+def calculate_for_page(browser, url) -> list:
     navigation_start = browser.execute_script("return window.performance.timing.navigationStart")
     response_start = browser.execute_script("return window.performance.timing.responseStart")
     dom_complete = browser.execute_script("return window.performance.timing.domComplete")
